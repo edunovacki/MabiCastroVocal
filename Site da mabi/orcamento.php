6496deff1c069a932d8010759e67085277780436
edunovacki <111133454+edunovacki@users.noreply.github.com>
2025-06-14 22:02:39 -0300

    Add files via upload
    Transformei em php;

---
+++ Site da mabi/orcamento.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+    <main>
+    <section class="repertorio-orcamento">
+            <div class="container-repertorio-orcamento text-center">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-center" data-aos="fade-down" data-aos-duration="2500">Orçamento</h1><hr data-aos="fade-down" data-aos-duration="2500">
+                        <p class="text-center lead" data-aos="fade-up" data-aos-duration="2500">Cada casamento é único - e o nosso serviço também. Por isso, não
+                        trabalhamos com pacotes fixos: montamos uma proposta sob medida, de acordo com o estilo do
+                        evento, duração, local e formação musical escolhida.</p><hr data-aos="fade-up" data-aos-duration="2500">
+                        <p class="text-center" data-aos="fade-up" data-aos-duration="2500">Como funciona?
+                        Você entra em contato com a gente
+                        (Pode ser pelo link, WhatsApp ou Instagram)
+                        Marcamos uma conversa rápida
+                        para entender o estilo do seu casamento, local e o que você está imaginando.
+                        Enviamos um orçamento personalizado
+                        com todas as informações e opções que se encaixam no seu perfil.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include 'footer.php'; ?>
---
+++ Site da mabi/repertorio.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+    <main>
+    <section class="repertorio-orcamento">
+            <div class="container-repertorio-orcamento text-center">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-center" data-aos="fade-down" data-aos-duration="2500">Repertório</h1><hr data-aos="fade-down" data-aos-duration="2500">
+                        <p class="text-center lead" data-aos="fade-up" data-aos-duration="2500">Cada casamento é único - e a trilha sonora também deve ser. Nosso grupo oferece um repertório versátil, feito sob medida para o estilo e as preferências do casal. Tocamos de tudo: de gospel a pop, de sertanejo a MPB, de clássicos do amor a hits atuais. A escolha é sua!</p><hr data-aos="fade-up" data-aos-duration="2500">
+                        <p class="text-center" data-aos="fade-up" data-aos-duration="2500">Nós adaptamos o repertório conforme o clima do evento, seja uma cerimônia intimista ou uma festa animada que vai até o fim da noite. Para escolher as músicas, começamos com uma reunião com o casal, em que conversamos sobre o estilo do casamento e os momentos especiais da cerimônia ou da festa. A partir disso, enviamos sugestões de repertório com base no gosto dos noivos e no tempo de apresentação. E o mais importante: vocês têm total liberdade para escolher as músicas que não podem faltar — e também aquelas que preferem evitar.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include 'footer.php'; ?>
